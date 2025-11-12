90f4a96239155ace590e2a11f15433cae369443c
victor256sd <victor256@gmail.com>
2025-11-11 18:41:45 -0800

    Update main.py

--- main.py
+++ main.py
@@ -123,12 +123,11 @@ if st.session_state.get('authentication_status'):
 
         st.markdown("#### Sources")
         # Extract annotations from the response, and print source files.
-        try:
-            annotations = response2.output[1].content[0].annotations
+        if annotations = response2.output[1].content[0].annotations:
             retrieved_files = set([response2.filename for response2 in annotations])
             file_list_str = ", ".join(retrieved_files)
             st.markdown(f"**File(s):** {file_list_str}")
-        except:
+        else:
             st.markdown("**File(s): n/a**")
 
         # st.session_state.ai_response = cleaned_response
