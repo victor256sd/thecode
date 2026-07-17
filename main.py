09df17603bd6a22f752adf7e46d82341f1527914
victor256sd <victor256@gmail.com>
2026-07-17 06:50:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ st.markdown("""
     <style>
            /* Remove blank space at top and bottom */ 
            .block-container {
-               padding-top: 1rem;
+               padding-top: 1.5rem;
                padding-bottom: 0rem;
            }
 
