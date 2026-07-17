6d4a8f1839c1de49153e46d629abf8811d6df56f
victor256sd <victor256@gmail.com>
2026-07-16 22:59:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ st.markdown("""
     fill: #1E90FF !important;
     stroke: #1E90FF !important;
 }
-[data-testid="stSidebarExpandButton"] * {
+[data-testid="stSidebarCollapsedControl"] * {
     color: #1E90FF !important;
     fill: #1E90FF !important;
     stroke: #1E90FF !important;
