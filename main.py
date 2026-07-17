8d5a27847acf4e54ea7786dea90838512e527335
victor256sd <victor256@gmail.com>
2026-07-16 21:55:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -52,6 +52,15 @@ st.markdown("Review NASDTEC Ethics Chatbot: [link](https://www.surveymonkey.com/
 #-------------------------------------------------------
 # Setup sidebar.
 #-------------------------------------------------------
+# Custom CSS for changing sidebar expand/collapse icon color
+custom_css = """
+<style>
+[data-testid="collapsedControl"] svg {
+color: #0068C9 !important; /* Replace with your desired color */
+}
+</style>
+"""
+st.markdown(custom_css, unsafe_allow_html=True)
 st.sidebar.title("Additional Information")
 st.sidebar.markdown("## Prompt Writing Pointers")
 with st.sidebar.expander("1. Start with your role and the educational setting.", expanded=False):
