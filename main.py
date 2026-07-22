b1ed6830bcdbd532addbadadfd7151eab97a96de
victor256sd <victor256@gmail.com>
2026-07-21 23:36:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ st.markdown("""
         left: 40px;
         padding: 55px;
         font-size: 24px;
-        z-index: 999999; /* Keep it above other elements */
+        z-index: 9999999; /* Keep it above other elements */
     }
 
     /* Hidden on desktop */
