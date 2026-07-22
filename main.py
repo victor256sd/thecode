86b13e33057b4f96f3a3e68391c900eb6aaa5ec8
victor256sd <victor256@gmail.com>
2026-07-22 00:03:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -130,11 +130,7 @@ st.markdown("""
     </div>
 """, unsafe_allow_html=True)
 
-st.sidebar.markdown("""
-    <h1 style="text-decoration: underline;">
-        More Information
-    </h1>
-    """, unsafe_allow_html=True)
+st.sidebar.title("**More Information")
 st.sidebar.markdown("## Prompt Writing Pointers")
 with st.sidebar.expander("1. Start with your role and the educational setting", expanded=False):
     st.markdown("""
